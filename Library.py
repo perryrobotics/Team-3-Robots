a827bc694112f87db9c1d824572dc70119ee003f
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2019-02-19 17:25:03 -0600

    The start of something awesome!!

---
+++ Library.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+import os, sys
+from wallaby import *
+Lmotor = 3
+Rmotor = 0
+    
+Arm = 3
+Arm_back = 2000
+Arm_up = 500
+Arm_down = 250
+    
+Claw = 2
+Claw_open = 2000
+Claw_close = 1100
+    
+Box = 0
+Box_back = 1100
+Box_down = 450
+
+Touch_port = 0
+Line_port = 0
+Thresh = 2750
+    
+def move(speed, ticks):
+	cmpc(Lmotor)
+	mav(Lmotor, speed);
+	mav(Rmotor, speed);
+	while gmpc(Lmotor) < ticks:
+		pass
+	ao()
+            
+def move_back(speed, ticks):
+	cmpc(Lmotor)
+	mav(Lmotor, -speed);
+	mav(Rmotor, -speed);
+	while gmpc(Lmotor) > -ticks:
+		pass
+	ao()
+
+def move_left(speed, ticks):
+	cmpc(Rmotor)
+  	cmpc(Lmotor)
+	mav(Lmotor,-speed)
+	mav(Rmotor, speed)
+	while gmpc(Rmotor) < ticks:
+		pass
+	ao()
+
+def move_right(speed, ticks):
+	cmpc(Lmotor)
+	mav(Lmotor, speed)
+	mav(Rmotor, -speed)
+	while gmpc(Lmotor) < ticks:
+		pass
+	ao()
+  
+def drive_to_black(speed):
+	mav(Rmotor, speed)
+  	mav(Lmotor, speed)
+  	while analog(Line_port) < Thresh:
+		pass
+	ao()
+            
+def drive_to_white(speed):
+	mav(Rmotor, speed)
+  	mav(Lmotor, speed)
+  	while analog(Line_port) > Thresh-1000:
+		pass
+	ao()    
+   
+def move_to_hit(speed):
+	mav(Rmotor, speed)
+  	mav(Lmotor, speed)
+	while digital(Touch_port) ==0:
+		pass
+	ao()
+            
+def right_to_black(speed):
+	mav(Lmotor, speed)
+	mav(Rmotor, -speed)
+	while analog(Line_port) < Thresh:
+		pass
+	ao()   
+            
+def move_servo_slow(port, start_pos, end_pos, step):
+	if  end_pos < start_pos:
+		step=-step
+  	for pos in range(start_pos, end_pos, step):
+		set_servo_position(port, pos)
+		msleep(50)
+ 	set_servo_position(port, end_pos)
+            
+##############################            
+
+def arm_back(step):
+	move_servo_slow(Arm, get_servo_position(Arm), Arm_back, step)
+	msleep(750)
+    
+def arm_up(step):
+	move_servo_slow(Arm, get_servo_position(Arm), Arm_up, step)
+
+def arm_down(step):
+	move_servo_slow(Arm, get_servo_position(Arm), Arm_down, step)
+
+
+#############################
+
+
+        
+def open_claw(step):
+	move_servo_slow(Claw, get_servo_position(Claw), Claw_open, step)
+   
+def close_claw(step):
+  	move_servo_slow(Claw, get_servo_position(Claw), Claw_close, step)
+
+        
+############################
+        
+def move_box(position):
+	set_servo_position(Box, position)
+	msleep(750)
+        
+def carry_box(step):
+	move_servo_slow(Box,get_servo_position(Box), Box_back, step)
+        
+def down_box(step):
+	move_servo_slow(Box,get_servo_position(Box), Box_down, step)
---
+++ main.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+import os, sys
+from wallaby import *
+from Library import *
+
+#start and get into start position
+enable_servos()
+arm_back(20)
+open_claw(20)
+carry_box(100)
+msleep(2000)
+#into start position!  GO!!
+print("START!!!")    
+drive_to_black(900)
+drive_to_white(900)
+drive_to_black(900)
+move_left(900,900)
+print("DONE!!!")
+#to hospital
+drive_to_black(900)  
+move_right(900, 700)
+
+#getting 1st ball
+arm_down(20)
+move(900,450)
+move_right(300,100)
+close_claw(35)
+move_back(900, 150)
+arm_back(50)
+open_claw(50)
+
+#next set of balls
+move_right(700,300)
+move(900,750)
+arm_down(50)
+move(900,400)
+move_right(150,250)
+msleep(1000)
+close_claw(50)
+arm_back(50)
+open_claw(50)
+    
+#set 3 of balls
+print("here")
+move_right(900,1)
+print("now here")
+arm_up(50)
+move(900,2000)
+arm_down(50)
+close_claw(50)
+move_right(1000,20)
+arm_back(50)
+open_claw(50)
+    
+#set 4
+move(900,2500)
+move_left(450,50)
+arm_down(50)
+move_right(450,150)
+close_claw(50)
+arm_back(50)
+open_claw(50)
+
+#final set
+print("final")
+move(900,2100)
+move_left(900,30)
+arm_down(50)
+move(900,700)
+close_claw(50)
+arm_back(50)
+open_claw(50)
+
+#going to dump balls
+move_right(990,100)
+move_to_hit(900)
+move(900,100)
+move_right(900,950)
+move_back(900,2000)
+    
+drive_to_black(900)
+msleep(100)
+drive_to_white(900)
+msleep(100)
+print("I didnt skip")
+drive_to_black(900)
+move_right(900,900)
+right_to_black(900)
+#dumping
+arm_up(50)
+move_left(900,450)
+move(900,200)
+down_box(40)
+for _ in range(20):
+	print("vibrate!!!")
+	set_servo_position(Box, 530)
+  	msleep(100)
+  	set_servo_position(Box, 450)
+	msleep(100)
+carry_box(50)
+move(900,200)
+arm_back(50)
+move_right(900,1700)
+move_back(900,1500)
+close_claw(50)
+arm_down(50)
+move_right(600, 375)
+arm_up(50)
+open_claw(50)
+arm_down(50)
+move(900,1100)
+  
+        
+        
+        
+        
+        
+        
+        
+        
+        
